a7ba1705279c63d4a879c08e7a959360d16e1341
babyshoes <dianecai@gmail.com>
2019-07-30 16:19:29 -0400

    move to react hooks

--- src/Staff.js
+++ src/Staff.js
@@ -1,20 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect, useLayoutEffect, useRef } from 'react';
 import { drawStaff } from './viz/staff'
 import './App.css';
 
-export default class Staff extends Component {   
-    
-    componentDidMount() {
-        drawStaff(this.props)
-    };
+export default function Staff ({timestep, numTimesteps, chords}) {
+    const [height, setHeight] = useState(0)
+    const ref = useRef(null)
 
-    componentDidUpdate() {
-        drawStaff(this.props)
-    };
+    useLayoutEffect(() => {
+        setHeight(ref.current.clientHeight)
+        drawStaff(ref, timestep, numTimesteps, chords, height)
+    })
 
-    render(){ 
-        return (
-            <div id="staff-viz"/>
-        );
-    }
-  }
+    // useEffect(() => {
+    //     drawStaff(ref, timestep, numTimesteps, chords, height)
+    // })
+
+    return (
+        <div ref={ref} id="staff-viz"/>
+    )
+
+} 
+ 
+// export default class Staff extends Component {   
+
+//     componentDidMount() {
+//         drawStaff(this.props)
+//     };
+
+//     componentDidUpdate() {
+//         drawStaff(this.props)
+//     };
+
+//     render(){ 
+//         return (
+//             <div id="staff-viz"/>
+//         );
+//     }
+//   }
